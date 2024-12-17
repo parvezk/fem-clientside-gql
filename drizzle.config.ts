78348ffd25e13f10cf641300c10bde9cbf47119a
Parvez Kose <parvez.kosex@gmail.com>
2024-12-17 14:21:16 -0500

    up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,7 +9,7 @@ export default {
     authToken: process.env.TURSO_AUTH_TOKEN!,
   }, */
   dbCredentials: {
-    url: 'libsql://my-db-parvezk.turso.io',
+    url: 'my-db-parvezk.turso.io',
     authToken:
       'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3Mjk2MDkwNjYsImlkIjoiMWNmNmQ1ZmMtY2E3NS00Nzk0LWFmMjAtMzhmNDkwMGM1ZmFlIn0.wiWt7av98z6o9Jvdu6i4HLKAd84tb3jtHYdiec8vl4WE9dAuggILMzhT5wfzozrgu10MDT4PNQVVnENwX7guCQ',
   },
